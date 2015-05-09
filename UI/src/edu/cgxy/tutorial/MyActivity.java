6091d62534269574bf10022d6b68cf04f9a3357c
zhouyang3 <zhouyang3@kingsoft.com>
2015-05-09 19:10:08 +0800

    之前的不知道为什么生成的apk有问题.

--- UI/src/edu/cgxy/tutorial/MyActivity.java
+++ UI/src/edu/cgxy/tutorial/MyActivity.java
@@ -3,12 +3,12 @@ package edu.cgxy.tutorial;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class MyActivity extends Activity
+{
+    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
